c1b9d04b46eb8227c1d3cc9f89638ecc56fc8ee6
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2025-11-12 13:47:48 -0300

    Os gets e sets de Produto, bem basikinha

---
+++ src/main/java/io/github/cannudo/produtosapi/model/Produto.java
@@ -0,0 +1,41 @@
+package io.github.cannudo.produtosapi.model;
+
+public class Produto {
+    private String id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
